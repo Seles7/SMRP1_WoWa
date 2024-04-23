27d0e1bd8ad1cbd6d5658b83e657a98d5553d3e4
Acer <seleznev18012003s@gmail.com>
2024-04-23 11:49:06 +0500

    com6

---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from .ui_main_window import Ui_MainWindow as UiMainWindow
+from .ui_create_user import Ui_CreateNewUser as UiCreateNewUser_Form
+from .ui_user_already_exists import Ui_CurrentUser as UiUserAlreadyExists_Form
+from .ui_store import Ui_Store as UiStore
+from .ui_purchase import Ui_Purchase as UiPurchase
+from .ui_purchase_error import Ui_Purchase_Error as UiPurchaseError
+from .ui_game_info import Ui_Game_info as UiGame_info
+from .ui_user_list import Ui_User_List as UiUser_List
+from .ui_review_window import Ui_Review as UiReview_Form
