c06ead49d6592d13c407e6fc213982bddf3617b0
Acer <seleznev18012003s@gmail.com>
2024-04-23 11:50:43 +0500

    Revert "com6"
    This reverts commit 27d0e1bd8ad1cbd6d5658b83e657a98d5553d3e4.

--- database/character.py
+++ database/character.py
@@ -12,7 +12,6 @@ class Character(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     nickname = Column(String(100), nullable=False)
     level = Column(Integer, nullable=False)
-    password = Column(String(50), nullable=False)
     id_racespec = Column(ForeignKey("RaceSpec.id"), primary_key=True)
     id_guild = Column(ForeignKey("Guild.id"), primary_key=True)
 
@@ -20,7 +19,7 @@ class Character(Base):
     guild = relationship("Guild", back_populates="guild_characters")
 
     def __str__(self):
-        return f"Character {self.id} {self.nickname} {self.level} {self.password} {self.id_racespec} {self.id_guild}"
+        return f"Character {self.id} {self.nickname} {self.level} {self.id_racespec} {self.id_guild}"
 
     def __repr__(self):
         return str(self)
