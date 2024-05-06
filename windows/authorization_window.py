98a469af20f550da6bfda6f6297630727b13ef28
Acer <seleznev18012003s@gmail.com>
2024-05-07 00:46:37 +0500

    com Ы

--- windows/authorization_window.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QDialog
-
-from ui import UiEntranceWindow, UiAuthorizationWindow
-
-from database import get_session, User
-
